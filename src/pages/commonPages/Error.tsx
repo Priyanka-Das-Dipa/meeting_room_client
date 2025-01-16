451994600b1d5835ba594d350e9511de59ed810e
Priyanka Das Dipa <priyankadipa9102@gmail.com>
2025-01-16 22:38:23 +0600

    Error page design done

--- src/pages/commonPages/Error.tsx
+++ src/pages/commonPages/Error.tsx
@@ -1,7 +1,21 @@
-
-
+import { Link } from "react-router-dom";
+import img from "../../../public/error.jpg";
 const Error = () => {
-  return <div>this is error page</div>;
+  return (
+    <div className="bg-black h-screen">
+      <div className="flex justify-center text-center">
+        <img src={img} alt="" />
+      </div>
+      <div className="flex justify-center items-center gap-5 pt-10">
+        <button className="text-white text-2xl font-bold border border-white px-6 rounded-md py-2 hover:text-primary">
+          <Link to="/">Home</Link>
+        </button>
+        <button className="text-white text-2xl font-bold border border-white px-6 rounded-md py-2 hover:text-primary">
+          <Link to="/login">Login</Link>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
